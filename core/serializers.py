27c4654bf3f2b340c56173d8ef582eadfb85c2e4
Ali Khodayari <alikhodayari1381ard@gmail.com>
2025-09-21 01:35:16 +0330

    Edit EmployeeSerializer and Update/Edit AttendanceSerializer and Update WorkScheduleSerializers

--- core/serializers.py
+++ core/serializers.py
@@ -11,31 +11,36 @@ class EmployeeSerializer(serializers.ModelSerializer):
         rep['نام'] = rep.pop('first_name')
         rep['نام خانوادگی'] = rep.pop('last_name')
         rep['کد کارمندی'] = rep.pop('employee_id')
-        rep['QR-code'] = rep.pop('qr_code')
+
+        if 'qr_code' in rep:
+            rep['QR-code'] = rep.pop('qr_code')
+        else:
+            rep['QR-code'] = None  
         return rep
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         request = self.context.get('request')
         if request and request.method != 'GET':
-            
+
             self.fields.pop('qr_code', None)
         
 
 class AttendanceSerializer(serializers.ModelSerializer):
     employee_name = serializers.CharField(source='employee', read_only=True)
-    late_minutes_display = serializers.SerializerMethodField()
+    employee_id = serializers.IntegerField(source='employee.id', read_only=True)
+    employee_code = serializers.CharField(source='employee.employee_id', read_only=True)
     class Meta:
         model = Attendance
-        fields = ['id','date' ,'check_in_time', 'status' ,'late_minutes_display','employee_name']
+        fields = ['id','date' ,'check_in_time', 'status' ,'late_minutes','employee_name','employee_id','employee_code']
 
         
-    def get_late_minutes_display(self, obj):
-        return f" {obj.late_minutes} دقیقه"
     
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data['کاربر'] = data.pop('employee_name')
+        data['کاربر_ایدی'] = data.pop('employee_id')
+        data['کد کارمندی'] = data.pop('employee_code')
         data['تاریخ'] = data.pop('date')
 
         if instance.check_in_time:
@@ -46,7 +51,7 @@ class AttendanceSerializer(serializers.ModelSerializer):
         data.pop('check_in_time', None)
         data['status'] = instance.get_status_display()
         data['وضعیت'] = data.pop('status')
-        data['دیر کرد'] = data.pop('late_minutes_display')
+        data['دیر کرد'] = data.pop('late_minutes')
 
         return data
     
@@ -54,7 +59,7 @@ class AttendanceSerializer(serializers.ModelSerializer):
 class WorkScheduleSerializers(serializers.ModelSerializer):
     class Meta:
         model = WorkSchedule
-        fields  =['start_time','end_time']
+        fields  =['id','start_time','end_time']
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
